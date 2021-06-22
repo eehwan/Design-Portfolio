ce8ee66dd6ac2abf070b39fd8b07fdbe8f7f5610
eehwan <eehwan.shin@gmail.com>
2021-06-22 21:43:59 +0900

    menu image - transition effect

--- js/menuIMG.js
+++ js/menuIMG.js
@@ -1,15 +1,16 @@
 const menu_container = document.querySelector(".menu_container");
 
+const img1 = document.querySelector("#right_area .one");
+img1.classList.add("on");
 Array.from(menu_container.children).forEach(x => {
     x.addEventListener('mouseenter', (e) => {
         const newImg = document.querySelector(`#right_area .${e.target.className}`)
-        newImg.style.display = "block"
+        newImg.classList.add("on");
     })
     x.addEventListener('mouseleave', (e) => {
         const newImg = document.querySelector(`#right_area .${e.target.className}`);
-        newImg.style.display = "none";
-        const img1 = document.querySelector("#right_area .one");
-        img1.style.display = "block";
+        newImg.classList.remove("on");
+        img1.classList.add("on");
     })
 });
 
