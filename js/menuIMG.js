7521fc290e32e5f20827bc48669026ad41e54ffc
eehwan <eehwan.shin@gmail.com>
2021-06-23 21:51:38 +0900

    menu section - link path

--- js/menuIMG.js
+++ js/menuIMG.js
@@ -14,3 +14,9 @@ Array.from(menu_container.children).forEach(x => {
     })
 });
 
+document.querySelector(".menu_container:nth-child(1)").addEventListener('click', () => {
+    location.replace('highJewerly.html');
+});
+document.querySelector(".menu_container:nth-child(1)").addEventListener('click', () => {
+    location.replace('bezero1.html');
+});
